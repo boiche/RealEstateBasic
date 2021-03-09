7c75346259ae0dce91f22895ecef873823106ad1
Boil Marinov <boil.marinov16@gmail.com>
2021-03-09 10:52:38 +0200

    Initial

---
+++ RealEstate/HouseDataModel.cs
@@ -0,0 +1,26 @@
+﻿namespace RealEstate
+{
+    public class HouseDataModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsSold { get; set; }
+        public decimal Price { get; set; }
+        public Distribution[] Distributions { get; set; }
+        public decimal BuildUpArea { get; set; }
+        public decimal Yard { get; set; }
+
+        public class Distribution
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public Section[] Sections { get; set; }
+
+            public class Section
+            {
+                public string Name { get; set; }
+                public decimal Area { get; set; }
+            }            
+        }        
+    }    
+}
---
+++ RealEstate/LoginHandler.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace RealEstate
+{
+    public static class LoginHandler
+    {
+        public static void RecordLogin()
+        {
+            RecordLoginModel model = new RecordLoginModel(DateTime.Now);
+            JsonConvert.SerializeObject(model);
+            Console.WriteLine("Logged in successfully!");
+        }
+
+        private class RecordLoginModel
+        {
+            public DateTime dateTimeOfLogin;
+            public RecordLoginModel(DateTime dateTime)
+            {
+                dateTimeOfLogin = dateTime;
+            }
+        }
+    }  
+}
